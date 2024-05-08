bda07194b36f9f17dbcfe0c11659eb0ca5811891
3nd-06 <battagliaalessandro2022@olivelliputelli.it>
2024-05-08 08:39:16 +0200

    pull su pc scuola

---
+++ src/main/java/com/mycompany/squadredicalcio2/SquadreDiCalcio2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.squadredicalcio2;
+
+/**
+ *
+ * @author Studente
+ */
+public class SquadreDiCalcio2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
